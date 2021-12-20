85c354f4b078c6c69cb8050bce7fde95f10bad40
zaki178 <74228332+zaki178@users.noreply.github.com>
2021-12-20 13:32:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,16 @@ apt-get install tesseract
 apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
+npm install @adiwajshing/baileys
+npm install @adiwajshing/baileys@3.5.2
+npm install @adiwajshing/baileys@3.5.3
+npm install @adiwajshing/baileys@3.5.1
+npm i ffmpeg
+npm i @adiwajshing/baileys@latest
 
 echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
+npm audit 
+npm audit fix
+npm fund
+yarn
+npm start
